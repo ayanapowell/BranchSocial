d727173775dbe1d8aa3539f7372eb12bd0e76dfa
Ayana Powell <ayanapowell1@gmail.com>
2016-11-07 13:25:07 -0800

    create a helper for date format

--- app/components/add-post.js
+++ app/components/add-post.js
@@ -3,12 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     savePost() {
-      // var date = moment().format('MMMM Do YYYY, h:mm a');
       var date = moment("20161106","YYYYMMDD").fromNow();
       var params = {
         post: this.get('post'),
         date: date
       }
+      console.log(params['date']);
       $('#add-post').trigger('reset');
       this.sendAction('savePost', params);
     }
