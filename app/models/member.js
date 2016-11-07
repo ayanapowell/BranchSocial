fb184d8768e56d1e2a76d3b0165733bd56f90103
chris martinez and Sheena Nickerson <sheena@>
2016-11-07 11:04:07 -0800

    add user id to member model

---
+++ app/models/member.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  firstName: DS.attr(),
+  lastName: DS.attr(),
+  screenName: DS.attr(),
+  city: DS.attr(),
+  state: DS.attr(),
+  zipCode: DS.attr(),
+  avatar: DS.attr(),
+  friends: DS.hasMany('member', {async:true}),
+  id: DS.attr()
+});
