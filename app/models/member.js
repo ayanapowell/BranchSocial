217eb5f03589272faab40fc0267a5d820a83870f
ayana powell and sheena nickerson <ayana+sheena@>
2016-11-08 13:33:07 -0800

    add a model for commenting on posts

--- app/models/member.js
+++ app/models/member.js
@@ -10,6 +10,6 @@ export default DS.Model.extend({
   avatar: DS.attr(),
   email: DS.attr(),
   // friends: DS.hasMany('member', {async:true}),
-  // posts: DS.hasMany('post', {async:true})
-  posts: DS.hasMany('post', {async:true})
+  posts: DS.hasMany('post',{async:true}),
+  comments: DS.hasMany('comments', {async:true})
 });
--- app/models/post.js
+++ app/models/post.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   date: DS.attr(),
   content: DS.attr(),
-  member: DS.belongsTo('member', {async:true})
+  member: DS.belongsTo('member', {async:true}),
+  comments: DS.hasMany('comment', {async: true})
 });
