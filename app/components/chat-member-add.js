9bed4867dba1760e5a3ffaff23c9a47f5044a24e
April Peng and Mark Lawson <april+mark@>
2016-11-07 16:34:38 -0800

    add search to chatroom for finding users

--- app/components/chat-member-add.js
+++ app/components/chat-member-add.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  searchedTerm: 'dog',
+  foundMember: null,
+
+  actions: {
+    searchName(){
+      var _this = this;
+      this.set('searchedTerm', this.get('searchTerm'));
+      this.get('store').query('member', {
+        orderBy: 'screenName',
+        equalTo: this.get('searchedTerm')
+      }).then(function(response){
+        console.log(response.get('firstObject').get('firstName'));
+        _this.set('foundMember', response.get('firstObject'));
+      });
+    }
+  }
 });
