556bb5ac3ead1675d7672f0b95b27b6fa008d8ee
Ayana Powell <ayanapowell1@gmail.com>
2016-11-07 16:46:46 -0800

    fixed errors

--- app/routes/members.js
+++ app/routes/members.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   session: Ember.inject.service(),
-  search: Ember.inject.service(),
   model(){
     return Ember.RSVP.hash({
       member: this.store.findRecord('member', this.get('session').get('currentUser').uid),
