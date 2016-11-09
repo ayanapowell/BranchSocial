71c460ba8202d901c79686750ad82459f0a2ea84
Katrina Hirsch and Kevin Johnston <katrina+kevin@>
2016-11-09 08:40:24 -0800

    rename path name for members route

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('members');
+  this.route('members', {path: '/dashboard'});
   this.route('member-profile', {path: '/member-profile/:member_id'});
   this.route('group', {path: '/group/:group_id'});
   this.route('friends');
