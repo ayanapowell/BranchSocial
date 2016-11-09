2133d9823b701ce1f4f8a0df1992998ffa229ff4
April Peng and Mark Lawson <april+mark@>
2016-11-09 14:06:34 -0800

    remove friend requests when accepting requests

--- app/components/members-list.js
+++ app/components/members-list.js
@@ -5,8 +5,8 @@ export default Ember.Component.extend({
     sendFriendRequest(listMember, member){
       this.sendAction("sendFriendRequest",listMember, member);
     },
-    addFriend(member, requester_id){
-      this.sendAction('addFriend', member, requester_id);
+    addFriend(member, requester_id, request){
+      this.sendAction('addFriend', member, requester_id, request);
     },
     removeFriend(listMember, member){
       this.sendAction('removeFriend', listMember, member);
