f551d6cf408b7fe338a15263fdad120882d341dc
April Peng and Mark Lawson <april+mark@>
2016-11-08 16:22:12 -0800

    add formatting and scroll to chat

--- app/components/chat-display.js
+++ app/components/chat-display.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isScrolledToBottom: true,
+  willUpdate() {
+    this._super(...arguments);
+
+    let box = this.$('#chatDisplay')[0];
+    let isScrolledToBottom = box.scrollTop + box.clientHeight === box.scrollHeight;
+
+    this.set('isScrolledToBottom', isScrolledToBottom);
+  },
+
+  didRender() {
+    this._super(...arguments);
+
+    if (this.get('isScrolledToBottom')) {
+      this.$('#chatDisplay')[0].scrollTop = this.$('#chatDisplay')[0].scrollHeight;
+    }
+  }
 });
