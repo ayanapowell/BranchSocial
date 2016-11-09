6613bd3a3084e2f20b43acf245f580b89d0be3e9
Katrina Hirsch and Kevin Johnston <katrina+kevin@>
2016-11-09 09:39:32 -0800

    refactor edit/delete functions

--- app/components/edit-member.js
+++ app/components/edit-member.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateButtonClicked: false,
   actions: {
+    updateMember(){
+      this.set('updateButtonClicked', true);
+    },
     editInfo(member){
       var params = {
         firstName: this.get('editFirstName'),
@@ -13,6 +17,7 @@ export default Ember.Component.extend({
         avatar: this.get('editAvatar'),
       };
       this.sendAction('editInfo',member, params);
+      this.set('updateButtonClicked', false);
     },
     destroyMember(member){
       this.sendAction('destroyMember', member);
